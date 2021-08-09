3bb57739f55dc3dd9de82a0a915883081ca167a0
Matheus Geiser <matheus.lgeiser@ambevtech.com.br>
2021-08-09 20:10:12 -0300

    criando layout da tela

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,10 +13,74 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: AppTextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: "Matheus",
+                      style: AppTextStyles.titleBoldBackground,
+                    ),
+                  ],
+                ),
+              ),
+              subtitle: Text(
+                "Mantenha as suas contas em dia",
+                style: AppTextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Container(),
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {},
+              color: AppColors.primary,
+            ),
+            GestureDetector(
+              onTap: () {
+                print("Clicou");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: AppColors.primary),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.description_outlined),
+              onPressed: () {},
+              color: AppColors.body,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
