e6b5746da3430928fc2904deab49f5e08016df42
Matheus Geiser <matheus.lgeiser@ambevtech.com.br>
2021-08-09 21:41:22 -0300

    finalizando layout do barcodeScannerPage

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
@@ -8,6 +9,11 @@ import 'shared/constants/routes.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
